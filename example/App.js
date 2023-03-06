1a9e3de6eb3d20b4522afe91ef9c39aeb4f21311
bilgee0119 <bilgeeen416@gmail.com>
2023-03-06 20:11:58 +0800

    03/06

--- example/App.js
+++ example/App.js
@@ -5,52 +5,29 @@ export default function App() {
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView style={styles.srcollbain}>
-                <View style={styles.greeting}>
-                    <Text style={styles.ogloo}>Өглөөний мэнд</Text>
-                    <Text style={styles.ogloomassage}></Text>
-                    <Text style={styles.neglogo}>&#128512;</Text>
-                    <Text style={styles.hoyrlogo}>&#128157;</Text>
-                    <Text style={styles.guravlogo}>&#128293;</Text>
-                </View>
+              <View style={styles.paddingg}>
+              <Text style={styles.texthel}>MY CV</Text>
+              </View>
             </ScrollView>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "red",
-  
-    },
-    srcollbain: {
-      backgroundColor: "pink",
+  container: {
+    
+  },
+   srcollbain: {
+    backgroundColor: "#008B8B",
+   },
+   paddingg: {
+    flex: 1,
+    margin: 100,
+    alignItems: "center",
+   },
+   texthel: {
+    fontSize: 24,
+    fontWeight: 100,
+   }
 
-    },
-    greeting: {
-      flexDirection: "row",
-      alignItems: "flex-end",
-      justifyContent: "space-around",
-    },
-    ogloomassage: {
-      fontSize: 24,
-      fontWeight: "700",
-      width: 250,
-      paddingLeft: 20,
-    },
-    neglogo: {
-      width: 100,
-      height: 50,
-      margin: 10,
-    },
-    hoyrlogo: {
-      width: 100,
-      height: 50,
-      margin: 10,
-    },
-    guravlogo: {
-      width: 100,
-      height: 50,
-      margin: 10,
-    },
   });
