89c40eabe02837c989c7a14efe60cbe7c0bbde7f
bilgee0119 <bilgeeen416@gmail.com>
2023-03-14 09:36:24 +0800

    03/14

--- example/App.js
+++ example/App.js
@@ -1,27 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Drawer } from "@react-navigation/drawer";
 
 const Stack = createStackNavigator();
 
 function HomeScreen({ navigation }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Миний ертөнцөд тавтай морил</Text>
+    <Drawer
+      open={isOpen}
+      onClose={() => setIsOpen(false)}
+      type="overlay"
+      content={
+        <View style={styles.drawerContent}>
+          <TouchableOpacity
+            style={styles.drawerItem}
+            onPress={() => navigation.navigate("Миний тухай")}
+          >
+            <Text style={styles.drawerItemText}>Миний тухай</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.drawerItem}
+            onPress={() => navigation.navigate("Мэргэжил")}
+          >
+            <Text style={styles.drawerItemText}>Мэргэжил</Text>
+          </TouchableOpacity>
+        </View>
+      }
+    >
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => setIsOpen(true)}>
+            <Image
+              source={require("./assets/favicon.png")}
+              style={styles.menuIcon}
+            />
+          </TouchableOpacity>
+          <Text style={styles.title}>Миний ертөнцөд тавтай морил</Text>
+        </View>
+        <View style={styles.content}>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: "#EF961B" }]}
+            onPress={() => navigation.navigate("Миний тухай")}
+          >
+            <Image
+              source={require("./assets/favicon.png")}
+              style={styles.buttonImage}
+            />
+            <Text style={styles.buttonText}>Миний тухай</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: "#E4B612" }]}
+            onPress={() => navigation.navigate("Мэргэжил")}
+          >
+            <Image
+              source={require("./assets/favicon.png")}
+              style={styles.buttonImage}
+            />
+            <Text style={styles.buttonText}>Мэргэжил</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.content}>
-        <TouchableOpacity style={[styles.button, { backgroundColor: '#EF961B' }]} onPress={() => navigation.navigate('Миний тухай')}>
-        <Image source={require("./assets/favicon.png")} style={styles.buttonImage} />
-          <Text style={styles.buttonText}>Миний тухай</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, { backgroundColor: '#E4B612' }]} onPress={() => navigation.navigate('Мэргэжил')}>
-        <Image source={require('./assets/favicon.png')} style={styles.buttonImage} />
-          <Text style={styles.buttonText}>Мэргэжил</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </Drawer>
   );
 }
 
@@ -45,8 +95,8 @@ function ContactScreen() {
 
 export default function App() {
   return (
-    <NavigationContainer >
-      <Stack.Navigator style={styles.footer}>
+    <NavigationContainer>
+      <Stack.Navigator>
         <Stack.Screen name="Нүүр" component={HomeScreen} />
         <Stack.Screen name="Миний тухай" component={AboutScreen} />
         <Stack.Screen name="Мэргэжил" component={ContactScreen} />
@@ -55,52 +105,66 @@ export default function App() {
   );
 }
 
+const window = Dimensions.get("window");
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#61C3DC',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  footer: {
-    backgroundColor: '#61C3DC',
-    color: '#fff',
+    backgroundColor: "#fff",
   },
   header: {
-    backgroundColor: '#61C3DC',
-    borderBottomWidth: 2,
-    borderBottomColor: '#fff',
-    padding: 20,
-    width: '100%',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#3D6DCC",
+    height: 80,
+    paddingTop: 20,
+    paddingLeft: 10,
+  },
+  title: {
+    fontSize: 20,
+    color: "#fff",
+    paddingLeft: 10,
+  },
+  menuIcon: {
+    width: 20,
+    height: 20,
+    tintColor: "#fff",
   },
   content: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    color: '#FFF',
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    flexDirection: 'row',
-    padding: 12,
-    borderRadius: 5,
-    margin: 10,
-    width: '80%',
-    alignItems: 'center',
-    backgroundColor: '#6B5B95',
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 20,
+    width: window.width * 0.8,
+    height: 60,
+    borderRadius: 30,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
   },
   buttonImage: {
-    width: 25,
-    height: 25,
+    width: 30,
+    height: 30,
     marginRight: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+  drawerContent: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingLeft: 10,
+  },
+  drawerItem: {
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  drawerItemText: {
+    fontSize: 18,
   },
 });
