17adc60d1eff370ed1fb196ec9678ba229fc3c5c
bilgee0119 <bilgeeen416@gmail.com>
2023-03-17 09:26:52 +0800

    03/17

--- example/screens/Homescreen.js
+++ example/screens/Homescreen.js
@@ -6,24 +6,69 @@ import {
   View,
   SafeAreaView,
   ScrollView,
+  TouchableOpacity,
+  Image,
 } from "react-native";
 
 export default function Hoyrdahi({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <View style={styles.second}>
-          <Button style={styles.buttun}
-            title="Миний мэргэжил"
-            onPress={() => navigation.navigate("Second")}
-          />
+        <View style={styles.cv}>
+          <TouchableOpacity>
+            <Image
+              source={require("../../example/assets/favicon.png")}
+              style={styles.pro}
+            ></Image>
+            <View></View>
+            <View></View>
+          </TouchableOpacity>
         </View>
-        <View>
-          <Button
-            title="Hoyr dahi huudas baih english"
-            onPress={() => navigation.navigate("Guravdahi")}
-          />
-          <StatusBar style="auto" />
+        <View style={styles.doodBack}>
+          <View style={styles.golloh}>
+            <Image
+              source={require("../../example/assets/1465569.png")}
+              style={styles.nuur}
+            ></Image>
+            <Text style={styles.proNer}>Г. Билүүн</Text>
+            <Text style={styles.proHuis}>21, Male</Text>
+          </View>
+          <View style={styles.second}>
+            <Image
+              source={require("../../example/assets/chess.png")}
+              style={styles.buuMed}
+            ></Image>
+            <Button
+              style={styles.buttun}
+              title="Мэргэжил"
+              onPress={() => navigation.navigate("Second")}
+            />
+          </View>
+          <View>
+           
+            <TouchableOpacity
+              style={styles.second}
+              title="Мэргэжил"
+              onPress={() => navigation.navigate("Second")}
+            >
+               <Image
+              source={require("../../example/assets/chess.png")}
+              style={styles.buuMed}
+            ></Image>
+            <Text style={{ color: 'white', fontSize: 16 }}>Мэргэжил</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.medeh}>
+            <Image
+              source={require("../../example/assets/volleyball-player.png")}
+              style={styles.buuMed}
+            ></Image>
+            <Button
+              title="Хувийн мэдээлэл"
+              onPress={() => navigation.navigate("Guravdahi")}
+            />
+            <StatusBar style="auto" />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -33,19 +78,69 @@ export default function Hoyrdahi({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
+    backgroundColor: "white",
   },
-  second: { 
-  backgroundColor: '#007bff',
-  paddingHorizontal: 16,
-  paddingVertical: 8,
-  borderRadius: 8,
+  second: {
+    alignSelf: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    width: "90%",
+    padding: 20,
+    paddingBottom: 22,
+    borderRadius: 10,
+    shadowOpacity: 80,
+    elevation: 15,
+    marginTop: 20,
   },
-  buttun: {
-  color: 'white',
-  fontSize: 18,
-  fontWeight: 'bold',
-  textAlign: 'center',
-
+  // pro: {
+  //   width: 420,
+  //   height: 150,
+  // },
+  cv: {
+    padding: 10,
+    width: "100%",
+    backgroundColor: "#5F9EA0",
+    height: 150,
+  },
+  nuur: {
+    width: 140,
+    height: 140,
+    borderRadius: 150,
+    marginTop: -70,
+  },
+  golloh: {
+    alignItems: "center",
+  },
+  proNer: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  proHuis: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "grey",
+  },
+  buuMed: {
+    width: 25,
+    height: 25,
+  },
+  medeh: {
+    alignSelf: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    width: "90%",
+    padding: 20,
+    paddingBottom: 22,
+    borderRadius: 10,
+    shadowOpacity: 80,
+    elevation: 15,
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  doodBack: {
+    backgroundColor: "#20B2AA",
+    height: 700,
   },
 });
