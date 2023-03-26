2fcc656ec7b96ca17890fc0ad2085f8803336baf
bilgee0119 <bilgeeen416@gmail.com>
2023-03-26 18:07:23 +0800

    03/26 update

--- example/App.js
+++ example/App.js
@@ -171,35 +171,3 @@
 
 //     }
 //   });
-
-  import Homescreen from "../example/screens/Homescreen";
-import Hoyrdahi from "./screens/Hoyrdahi";
-import Gurawdugaar from "./screens/Gurawdugaar";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-const Stack = createNativeStackNavigator();
-  
-  export default function App() {
-    return (
-   <NavigationContainer>
-    <Stack.Navigator>
-        <Stack.Screen 
-            name="Home"
-            component={Homescreen}
-            options={{title: "Буцах"}}
-        />
-        <Stack.Screen
-          name="Second"
-          component={Hoyrdahi}
-        />
-        <Stack.Screen
-          name="Guravdahi"
-          component={Gurawdugaar}
-          options={{headerShown: false}}
-        />
-    </Stack.Navigator>
-   </NavigationContainer>
-    )
-};
-  
